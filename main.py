0322c8d3b4db8241a575b0bb8c1ba1ebeb551447
binwifely <115625431+binwifely@users.noreply.github.com>
2023-05-27 19:26:53 +0300

    крек

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from pygame import*
 from button import Button
 from sprite import Player
+from random import randint
+from sprite import Human
 window = display.set_mode((500, 500))
 clock = time.Clock()
+kills = 0
 
 game = True
 run = False
 road = image.load('road.png')
+font.init()
+font = font.SysFont('Helvetica', 24)
 play = Button(250, 100, 200, 50, 'play_button.png')   
 exit = Button(250, 250, 200, 50, 'exit_button.png')
 
 
 car = Player('car.png', 250, 10, 50, 100, 5)
+human1 = Human('human1.png', 30, 100, 30, 30, randint(1,3))
+human2 = Human('human2.png', 470, 250, 30, 30, randint(1,3))
+human3 = Human('human3.png', 30, 400, 30, 30, randint(1,3))
+
+humans = [human1, human2, human3]
 
 road = transform.scale(road, (500, 500))
 road_width = road.get_width()
@@ -38,8 +48,21 @@ while game:
         if abs(scroll) > road_width:
             scroll = 0
         
+        for i in humans:
+            if i.rect.colliderect(car.rect):
+                i.rect.x = 1000
+                kills += 1
+
+        points = font.render('вбивства:' + str(kills), True, (100, 0, 255))
+        window.blit(points, (100, 50))
         car.draw(window)
         car.move()
+        human1.draw(window)
+        human1.moving()
+        human2.draw(window)
+        human2.moving()
+        human3.draw(window)
+        human3.moving()
     else:
         window.fill((0, 0, 0))
         if play.draw(window):
